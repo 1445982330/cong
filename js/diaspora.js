5d869c518dc06cb64c16147b74afa506b6f529c0
1445982330 <1445982330@qq.com>
2021-12-02 16:45:50 +0800

    Site updated: 2021-12-02 16:45:43

--- js/diaspora.js
+++ js/diaspora.js
@@ -417,15 +417,15 @@ $(function() {
 					{typed.destroy(); typed = null;}
 				else{
 					if($("#hitokoto").data('st') == true){
-						$.get("https://v1.hitokoto.cn/", function (data) {
+						$.get("https://v1.hitokoto.cn/?c=a&c=b&c=c", function (data) {
 						var data = data;
-						var str =  data.hitokoto + " ——  By "		
+						var str =  data.hitokoto + " ——  节选自 "		
 						var options = {
 						  strings: [ 
 							//str + "Who??^1000",
 							//str + "It's me^2000",
 							//str +'Haha, make a joke',
-							str + data.from,
+							str + "《" + data.from + "》",
 						  ],
 						  typeSpeed: 90,
 						  startDelay: 500,
